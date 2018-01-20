74c6552a03dd8fec1ef94324d4f2e8c6daa99fbd
Richard Morgan <mba.rick@gmail.com>
2018-01-20 10:44:41 -0600

    Add project files.

---
+++ ACM.BL/Customer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ACM.BL
+{
+    public class Customer
+    {
+		private string _lastname;
+		public string LastName
+		{
+			get
+			{
+				return _lastname;
+			}
+			set
+			{
+				_lastname = value;
+			}
+		}
+
+		public string FirstName { get; set; }
+
+		public string EmailAddress { get; set; }
+
+		public int CustomerId { get; private set; }
+
+		public string FullName
+		{
+			get
+			{
+				return LastName + "," + FirstName;
+			}
+		}
+    }
+
+
+}
